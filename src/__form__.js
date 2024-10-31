efc44f49542944b408d59a56284554c51b4b8ee2
Jahid458 <jahidul898@outlook.com>
2024-10-31 08:05:18 +0600

    ref form

--- src/__form__.js
+++ src/__form__.js
@@ -1,7 +1,9 @@
 /**
  * 1. use form tag and get value  onSubmit handeler with event (e)
  * From e access e.target.[name of the input feild].value
- *  2.use indivisual feild set for each feild
- *  3. 
+ *  2.controlled Element: use indivisual feild set for each input feild
+ *  3. controlled Element: one object in a state  holding all te input feilds value
+ * 4. uncontrolled element: useref to create a reference element and access value by like namref.current.value
+ * 
  * 
  */
