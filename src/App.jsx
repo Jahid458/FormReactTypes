74f97b6eaba9a0bb1e162bd897d7084555ae17a5
Jahid458 <jahidul898@outlook.com>
2024-10-31 11:03:24 +0600

    create some list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 
 import "./App.css";
-import ReusuableForm from "./components/ReusuableForm/ReusuableForm";
+import GrandPa from "./components/GrandPa/GrandPa";
+// import ReusuableForm from "./components/ReusuableForm/ReusuableForm";
 // import HookForm from "./components/HookForm/HookForm";
 // import RefForm from "./components/RefForm/RefForm";
 // import StateFullForm from "./components/StateFullForm/StateFullForm";
 // import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
-  const handleSignUpSubmit = data =>{
-    console.log('sign up data',data)
-  }
+  // const handleSignUpSubmit = data =>{
+  //   console.log('sign up data',data)
+  // }
 
-  const handleUpdateProfile = data =>{
-    console.log('Update Profile data',data)
-  }
+  // const handleUpdateProfile = data =>{
+  //   console.log('Update Profile data',data)
+  // }
 
 
   return (
     <>
         <h1>Form Master</h1>
+        <GrandPa></GrandPa>
         {/* <SimpleForm></SimpleForm> */}
         {/* <StateFullForm></StateFullForm> */}
           {/* <RefForm></RefForm> */}
           {/* <HookForm></HookForm> */}
-          <ReusuableForm fromTitle={'sign Up'} handleSubmit={handleSignUpSubmit}>
+          {/* <ReusuableForm fromTitle={'sign Up'} handleSubmit={handleSignUpSubmit}>
               <div>
                 <h2>Sign Up</h2>
                 <p>Please Sign Up right Now</p>
@@ -36,7 +38,7 @@ function App() {
                 <h2>Update Profile</h2>
                 <p>Always keep your Update Profile</p>
               </div>
-          </ReusuableForm>
+          </ReusuableForm> */}
 
 
     </>
